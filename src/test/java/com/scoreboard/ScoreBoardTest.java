49f2cb481a81dbc12542cd9838b32552e85b5f14
matej-rakar <matej.rakar@hpe.com>
2024-08-15 11:50:18 +0200

    AAA, more tests, refactoring

--- src/test/java/com/scoreboard/ScoreBoardTest.java
+++ src/test/java/com/scoreboard/ScoreBoardTest.java
@@ -1,6 +1,9 @@
 package com.scoreboard;
 
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.time.Instant;
 import java.util.LinkedHashMap;
 
 import static com.scoreboard.TestConstants.*;
@@ -12,7 +15,9 @@ public class ScoreBoardTest {
     public void addMatch_ValidParam_NoError() {
         ScoreBoard scoreBoard = new ScoreBoard();
         Match match = new Match(TEST_HOME_TEAM_NAME, TEST_AWAY_TEAM_NAME);
+
         scoreBoard.addMatch(match);
+
         assertTrue(scoreBoard.getScoreBoard().containsKey(match.hashCode()));
         assertEquals(scoreBoard.getScoreBoard().get(match.hashCode()), match);
     }
@@ -20,6 +25,7 @@ public class ScoreBoardTest {
     @Test
     public void addMatch_InvalidParam_ExceptionThrown() {
         ScoreBoard scoreBoard = new ScoreBoard();
+
         assertThrows(IllegalArgumentException.class, () -> scoreBoard.addMatch(null));
     }
 
@@ -28,14 +34,18 @@ public class ScoreBoardTest {
         ScoreBoard scoreBoard = new ScoreBoard();
         Match match = new Match(TEST_HOME_TEAM_NAME, TEST_AWAY_TEAM_NAME);
         scoreBoard.addMatch(match);
+
         Match matchFromScoreBoard = scoreBoard.getMatch(match.hashCode());
+
         assertEquals(matchFromScoreBoard, match);
     }
 
     @Test
     public void getMatch_MatchDoesNotExist_ReturnsNull() {
         ScoreBoard scoreBoard = new ScoreBoard();
+
         Match matchFromScoreBoard = scoreBoard.getMatch(123);
+
         assertNull(matchFromScoreBoard);
     }
 
@@ -44,12 +54,14 @@ public class ScoreBoardTest {
         ScoreBoard scoreBoard = new ScoreBoard();
         Match match = new Match(TEST_HOME_TEAM_NAME, TEST_AWAY_TEAM_NAME);
         scoreBoard.addMatch(match);
+
         scoreBoard.removeMatch(match.hashCode());
+
         assertTrue(scoreBoard.getScoreBoard().isEmpty());
     }
 
     @Test
-    public void getSummaryOfMatches_MultipleMatches_ReturnsOrderedList() {
+    public void getSummaryOfMatches_MultipleMatches_ReturnsOrderedMap() {
         ScoreBoard unorderedScoreBoard = new ScoreBoard();
         Match match1 = new Match(TEST_HOME_TEAM_NAME, TEST_AWAY_TEAM_NAME);
         Match match2 = new Match(TEST_HOME_TEAM_NAME_2, TEST_AWAY_TEAM_NAME_2);
@@ -64,7 +76,79 @@ public class ScoreBoardTest {
         expectedOrderedSummary.put(match2.hashCode(), match2);
         expectedOrderedSummary.put(match1.hashCode(), match1);
         expectedOrderedSummary.put(match3.hashCode(), match3);
-        assertIterableEquals(unorderedScoreBoard.getSummaryOfMatches().entrySet(), expectedOrderedSummary.entrySet(), "The LinkedHashMaps must be equal");
+
+        LinkedHashMap<Integer, Match>  actualOrderedSummary = unorderedScoreBoard.getSummaryOfMatches();
+
+        assertIterableEquals(actualOrderedSummary.entrySet(), expectedOrderedSummary.entrySet(), "The LinkedHashMaps must be equal");
+    }
+
+    @Test
+    public void getSummaryOfMatches_SingleMatch_ReturnsMapSingleEntry() {
+        ScoreBoard unorderedScoreBoard = new ScoreBoard();
+        Match match1 = new Match(TEST_HOME_TEAM_NAME, TEST_AWAY_TEAM_NAME);
+        unorderedScoreBoard.addMatch(match1);
+        LinkedHashMap<Integer, Match> expectedOrderedSummary = new LinkedHashMap<>();
+        expectedOrderedSummary.put(match1.hashCode(), match1);
+
+        LinkedHashMap<Integer, Match>  actualOrderedSummary = unorderedScoreBoard.getSummaryOfMatches();
+
+        assertIterableEquals(actualOrderedSummary.entrySet(), expectedOrderedSummary.entrySet(), "The LinkedHashMaps must be equal");
+    }
+
+    @Test
+    public void getSummaryOfMatches_MultipleMatchesTiedTotalScore_ReturnsMapInitialOrder() {
+        ScoreBoard unorderedScoreBoard = new ScoreBoard();
+        Match match1 = new Match(TEST_HOME_TEAM_NAME, TEST_AWAY_TEAM_NAME);
+        Match match2 = new Match(TEST_HOME_TEAM_NAME_2, TEST_AWAY_TEAM_NAME_2);
+        Match match3 = new Match(TEST_HOME_TEAM_NAME_3, TEST_AWAY_TEAM_NAME_3);
+        unorderedScoreBoard.addMatch(match1);
+        unorderedScoreBoard.addMatch(match2);
+        unorderedScoreBoard.addMatch(match3);
+        LinkedHashMap<Integer, Match> expectedOrderedSummary = new LinkedHashMap<>();
+        expectedOrderedSummary.put(match1.hashCode(), match1);
+        expectedOrderedSummary.put(match2.hashCode(), match2);
+        expectedOrderedSummary.put(match3.hashCode(), match3);
+
+        LinkedHashMap<Integer, Match>  actualOrderedSummary = unorderedScoreBoard.getSummaryOfMatches();
+
+        assertIterableEquals(actualOrderedSummary.entrySet(), expectedOrderedSummary.entrySet(), "The LinkedHashMaps must be equal");
+    }
+
+    @Test
+    public void getSummaryOfMatches_MultipleMatchesSameTotalScoreAndTime_ReturnsMapInitialOrder() throws NoSuchFieldException, IllegalAccessException {
+        ScoreBoard unorderedScoreBoard = new ScoreBoard();
+        Match match1 = new Match(TEST_HOME_TEAM_NAME, TEST_AWAY_TEAM_NAME);
+        Match match2 = new Match(TEST_HOME_TEAM_NAME_2, TEST_AWAY_TEAM_NAME_2);
+        Match match3 = new Match(TEST_HOME_TEAM_NAME_3, TEST_AWAY_TEAM_NAME_3);
+        match1.updateScore(1,2);
+        match2.updateScore(3,0);
+        match3.updateScore(2,1);
+        Field startTimeField = Match.class.getDeclaredField("startTime");
+        startTimeField.setAccessible(true);
+        Instant testTime = Instant.now();
+        startTimeField.set(match1, testTime);
+        startTimeField.set(match1, testTime);
+        startTimeField.set(match1, testTime);
+        unorderedScoreBoard.addMatch(match1);
+        unorderedScoreBoard.addMatch(match2);
+        unorderedScoreBoard.addMatch(match3);
+        LinkedHashMap<Integer, Match> expectedOrderedSummary = new LinkedHashMap<>();
+        expectedOrderedSummary.put(match1.hashCode(), match1);
+        expectedOrderedSummary.put(match2.hashCode(), match2);
+        expectedOrderedSummary.put(match3.hashCode(), match3);
+
+        LinkedHashMap<Integer, Match>  actualOrderedSummary = unorderedScoreBoard.getSummaryOfMatches();
+
+        assertIterableEquals(actualOrderedSummary.entrySet(), expectedOrderedSummary.entrySet(), "The LinkedHashMaps must be equal");
+    }
+
+    @Test
+    public void getSummaryOfMatches_NoMatches_ReturnsEmptyMap() {
+        ScoreBoard unorderedScoreBoard = new ScoreBoard();
+
+        LinkedHashMap<Integer, Match>  actualOrderedSummary = unorderedScoreBoard.getSummaryOfMatches();
+
+        assertTrue(actualOrderedSummary.entrySet().isEmpty());
     }
 
 }
